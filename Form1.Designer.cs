2f29c7c46ac7a4d1f45f4f50af559bf736fa8432
Ennadifi-2020 <58331424+Ennadifi-2020@users.noreply.github.com>
2021-01-14 20:03:02 +0100

    gestion clients

---
+++ Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿
+namespace gestion_clients
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.lastname = new System.Windows.Forms.TextBox();
+            this.adresse = new System.Windows.Forms.TextBox();
+            this.ville = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.villesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.gestion_cltDataSet = new gestion_clients.gestion_cltDataSet();
+            this.villesBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.villesTableAdapter = new gestion_clients.gestion_cltDataSetTableAdapters.villesTableAdapter();
+            this.btn_trie = new System.Windows.Forms.Button();
+            this.cbville = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.villesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gestion_cltDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.villesBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // id
+            // 
+            this.id.Location = new System.Drawing.Point(94, 53);
+            this.id.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.id.Name = "id";
+            this.id.Size = new System.Drawing.Size(137, 22);
+            this.id.TabIndex = 0;
+            this.id.TextChanged += new System.EventHandler(this.id_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ID :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(94, 92);
+            this.name.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(137, 22);
+            this.name.TabIndex = 3;
+            this.name.TextChanged += new System.EventHandler(this.name_TextChanged);
+            // 
+            // lastname
+            // 
+            this.lastname.Location = new System.Drawing.Point(94, 135);
+            this.lastname.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lastname.Name = "lastname";
+            this.lastname.Size = new System.Drawing.Size(137, 22);
+            this.lastname.TabIndex = 4;
+            // 
+            // adresse
+            // 
+            this.adresse.Location = new System.Drawing.Point(389, 60);
+            this.adresse.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.adresse.Name = "adresse";
+            this.adresse.Size = new System.Drawing.Size(137, 22);
+            this.adresse.TabIndex = 5;
+            // 
+            // ville
+            // 
+            this.ville.Location = new System.Drawing.Point(389, 130);
+            this.ville.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ville.Name = "ville";
+            this.ville.Size = new System.Drawing.Size(137, 22);
+            this.ville.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Location = new System.Drawing.Point(15, 216);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(271, 196);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(155, 116);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(89, 46);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Mise à jour";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(25, 39);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 43);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Ajouter";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Blue;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(25, 116);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 46);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Modifier";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(155, 39);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 43);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Supprimer";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(312, 206);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(556, 216);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "name :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Last name :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(309, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 17);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "adresse :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(309, 135);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 17);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "ville :";
+            // 
+            // villesBindingSource
+            // 
+            this.villesBindingSource.DataMember = "villes";
+            this.villesBindingSource.DataSource = this.gestion_cltDataSet;
+            // 
+            // gestion_cltDataSet
+            // 
+            this.gestion_cltDataSet.DataSetName = "gestion_cltDataSet";
+            this.gestion_cltDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // villesBindingSource1
+            // 
+            this.villesBindingSource1.DataMember = "villes";
+            this.villesBindingSource1.DataSource = this.gestion_cltDataSet;
+            // 
+            // villesTableAdapter
+            // 
+            this.villesTableAdapter.ClearBeforeFill = true;
+            // 
+            // btn_trie
+            // 
+            this.btn_trie.BackColor = System.Drawing.Color.Yellow;
+            this.btn_trie.Location = new System.Drawing.Point(648, 117);
+            this.btn_trie.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btn_trie.Name = "btn_trie";
+            this.btn_trie.Size = new System.Drawing.Size(116, 40);
+            this.btn_trie.TabIndex = 19;
+            this.btn_trie.Text = "Filtrer";
+            this.btn_trie.UseVisualStyleBackColor = false;
+            this.btn_trie.Click += new System.EventHandler(this.btn_trie_Click);
+            // 
+            // cbville
+            // 
+            this.cbville.DropDownWidth = 300;
+            this.cbville.FormattingEnabled = true;
+            this.cbville.Items.AddRange(new object[] {
+            "casa",
+            "youssoufia",
+            "agadir",
+            "safi",
+            "marrakech",
+            "tanger",
+            "chichawa"});
+            this.cbville.Location = new System.Drawing.Point(611, 60);
+            this.cbville.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cbville.Name = "cbville";
+            this.cbville.Size = new System.Drawing.Size(192, 24);
+            this.cbville.TabIndex = 20;
+            this.cbville.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(901, 445);
+            this.Controls.Add(this.cbville);
+            this.Controls.Add(this.btn_trie);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.ville);
+            this.Controls.Add(this.adresse);
+            this.Controls.Add(this.lastname);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.id);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.villesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gestion_cltDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.villesBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.TextBox lastname;
+        private System.Windows.Forms.TextBox adresse;
+        private System.Windows.Forms.TextBox ville;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private gestion_cltDataSet gestion_cltDataSet;
+        private System.Windows.Forms.BindingSource villesBindingSource;
+        private gestion_cltDataSetTableAdapters.villesTableAdapter villesTableAdapter;
+        private System.Windows.Forms.BindingSource villesBindingSource1;
+        private System.Windows.Forms.Button btn_trie;
+        private System.Windows.Forms.ComboBox cbville;
+    }
+}
+
